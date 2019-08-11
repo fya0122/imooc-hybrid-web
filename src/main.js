b77ec38e83e3ebe024f73cadaf0278f59a8c8354
fya0122 <18730930122@163.com>
2019-08-11 20:46:54 +0800

    '全部初始化ok'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@js/htmlFontSize.js'
+import '@css/reset.css'
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="about"><h1 class="box">This is an about page</h1></div>
 </template>
+<style scoped lang="scss">
+@import '@css/style.scss';
+.box {
+  font-size: px2rem(20);
+  color: $mainColor;
+}
+</style>
