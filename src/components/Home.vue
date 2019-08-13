25eb12b907f97842c85cc419231bff08f9b692db
fya0122 <18730930122@163.com>
2019-08-13 23:13:29 +0800

    '先提交下吧'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="home">Home.vue</div>
+  <div class="home">
+    <div class="home-content"><my-swiper :swiperImgs="swiperImgs" :height="swiperHeight"></my-swiper></div>
+  </div>
 </template>
 <script>
+import MySwiper from './swiper/MySwiper.vue';
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    MySwiper
+  },
+  data () {
+    return {
+      swiperImgs: [
+        require('@img/swiper-1.jpg'),
+        require('@img/swiper-2.jpg'),
+        require('@img/swiper-3.jpg'),
+        require('@img/swiper-4.jpg'),
+        require('@img/swiper-5.jpg'),
+        require('@img/swiper-6.jpg'),
+        require('@img/swiper-7.jpg'),
+        require('@img/swiper-8.jpg')
+      ],
+      swiperHeight: '184px'
+    };
+  }
 };
 </script>
 <style lang="scss">
@@ -11,5 +32,8 @@ export default {
   width: 100%;
   height: 100%;
   font-size: 16px;
+  &-content {
+    height: 100%;
+  }
 }
 </style>
--- src/components/swiper/MySwiper.vue
+++ src/components/swiper/MySwiper.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>MySwiper.vue</div>
+  <!-- 需要一个swiper的容器 -->
+  <swiper :options="swiperOptions">
+    <swiper-slide v-for="(item, index) in swiperImgs" :key="index"><img :style="{ height: height }" class="swiper-slide-img" :src="item" alt="" /></swiper-slide>
+    <template v-slot:pagination>
+      <div class="swiper-pagination"></div>
+    </template>
+  </swiper>
 </template>
 <script>
 import 'swiper/dist/css/swiper.css';
 import { swiper, swiperSlide } from 'vue-awesome-swiper';
 export default {
   name: 'MySwiper',
+  props: {
+    height: {
+      type: String,
+      default: 'auto'
+    },
+    swiperImgs: {
+      type: Array,
+      required: true,
+      default(){
+        return []
+      }
+    }
+  },
+  data(){
+    return {
+      swiperOptions: {
+        autoplay: 3500,
+        autoHeight: true
+      }
+    }
+  },
   components: {
     swiper,
     swiperSlide
