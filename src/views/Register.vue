6f872e45424125e9f943f85142842956b332eec3
fya0122 <18730930122@163.com>
2019-09-18 23:39:02 +0800

    '调用了native端的注册'

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -24,6 +24,7 @@
 </template>
 <script>
 import NavigationBar from '@c/currency/NavigationBar.vue'
+import md5 from '@js/md5.min.js'
 export default {
   name: 'register',
   components: {
@@ -33,7 +34,8 @@ export default {
     return {
       username: '',
       password: '',
-      confirmPassword: ''
+      confirmPassword: '',
+      md5Password: ''
     }
   },
   methods: {
@@ -49,9 +51,53 @@ export default {
         alert('请完善密码')
         return false
       }
+      // md5加密后的密码
+      this.md5Password = md5(this.password)
       /**
-       * 与原生进行交互，这里应该是保存用户输入的用户名和密码
+       * @判断当前的项目是运行在Android还是ios中，差别是：Android只能接收基本类型的，ios可以是对象的
        * */
+      if (window.androidJSBridge) {
+        // Android
+        this.onRegisterToAndroid()
+      } else if (window.webkit) {
+        // ios
+        this.onRegisterToIOS()
+      }
+    },
+    // Android方法
+    onRegisterToAndroid () {
+      const userJSON = JSON.stringify({
+        'username': this.username,
+        'password': this.md5Password
+      })
+      // 调用Android注册方法
+      const result = window.androidJSBridge.register(userJSON)
+      this.onRegisterCallback(result)
+    },
+    // ios方法
+    onRegisterToIOS () {
+      // ios注册是接收对象类型的
+      const userObj = {
+        'username': this.username,
+        'password': this.md5Password
+      }
+      /**
+       * ios不能直接返回返回值
+       * 所以ios操作完成之后会回调对应的回调方法
+       * 同时原生调用JS的方法只能使调用绑定到window对象中的方法
+       * 所以我们需要把ios操作完成之后的回调方法(registerCallback)绑定到window对象下
+       * */
+      window.registerCallback = this.onRegisterCallback
+      window.webkit.messageHandlers.register.postMessage(userObj)
+    },
+    // 处理native的返回值
+    onRegisterCallback (result) {
+      if (result) {
+        alert('注册成功')
+        this.onBackClick()
+      } else {
+        alert('注册失败, 请重试!')
+      }
     }
   }
 }
